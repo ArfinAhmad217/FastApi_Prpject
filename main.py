feffbacdfb2c22310c3f362038c4571adb399246
Arfin Ahmad <arfin.ahmad@mintwaystech.com>
2026-05-01 00:56:38 +0530

    FastAPI Groq integration complete

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from pydantic import BaseModel
+from ai_service import ask_ai
 
 app = FastAPI()
+
+class QuestionRequest(BaseModel):
+    question: str
 
 @app.get("/")
 def home():
@@ -9,3 +14,9 @@ def home():
 @app.get("/user/{id}")
 def get_user(id: int):
     return {"id": id}
+
+
+@app.post("/ask")
+async def ask(body: QuestionRequest):
+    answer = ask_ai(body.question)
+    return {"answer": answer}
